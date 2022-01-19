80f8aff700211e5718b99de267e2fea6fbe98b30
Oleksii Kraievyi <oleksiikraieviy@gmail.com>
2022-01-19 18:37:25 +0200

    fixes config applying + ca.key permissions

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-chmod -R 777 ./master/*
-chmod -R 777 ./slave1/*
-chmod -R 777 ./slave2/*
+chmod 0444 ./master/conf/*
+chmod 0444 ./slave1/conf/*
+chmod 0444 ./slave2/conf/*
 
 docker-compose up --build -d
 
@@ -14,21 +14,23 @@ docker exec mysql_master sh -c "export MYSQL_PWD=111; mysql -u root -e '$priv_st
 
 echo "Enabled replication on master node"
 
-# docker-ip() {
-#     docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$@"
-# }
+docker-ip() {
+    docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$@"
+}
 
-# MS_STATUS=`docker exec mysql_master sh -c 'export MYSQL_PWD=111; mysql -u root -e "SHOW MASTER STATUS"'`
-# CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
-# CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
+MS_STATUS=`docker exec mysql_master sh -c 'export MYSQL_PWD=111; mysql -u root -e "SHOW MASTER STATUS"'`
+CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
+CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
 
-# start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(docker-ip mysql_master)',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
-# start_slave_cmd='export MYSQL_PWD=111; mysql -u root -e "'
-# start_slave_cmd+="$start_slave_stmt"
-# start_slave_cmd+='"'
+start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(docker-ip mysql_master)',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
+start_slave_cmd='export MYSQL_PWD=111; mysql -u root -e "'
+start_slave_cmd+="$start_slave_stmt"
+start_slave_cmd+='"'
 
-# docker exec mysql_slave1 sh -c "$start_slave_cmd"
-# docker exec mysql_slave1 sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+echo $start_slave_cmd
 
-# docker exec mysql_slave2 sh -c "$start_slave_cmd"
-# docker exec mysql_slave2 sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+docker exec mysql_slave1 sh -c "$start_slave_cmd"
+docker exec mysql_slave1 sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+
+docker exec mysql_slave2 sh -c "$start_slave_cmd"
+docker exec mysql_slave2 sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
